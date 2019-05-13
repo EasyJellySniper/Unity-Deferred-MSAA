ef3d38ccfa742d2484831c50cb783c57fcedfb70
Squall Liu <ltes50414@gmail.com>
2019-05-13 11:39:38 +0800

    Add Editor Code
    Add editor code for scene view problem.

---
+++ DeferredMSAA_Transfer/DeferredMSAAEditor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+
+/// <summary>
+/// deferred aa editor
+/// </summary>
+[CustomEditor(typeof(DeferredMSAA))]
+public class DeferredMSAAEditor : Editor
+{
+    bool isSceneView = false;
+
+    void OnEnable()
+    {
+        EditorApplication.update -= CheckSceneView;
+        EditorApplication.update += CheckSceneView;
+    }
+
+    void OnDisable()
+    {
+        EditorApplication.update -= CheckSceneView;
+    }
+
+    void OnSceneGUI()
+    {
+        isSceneView = true;
+    }
+
+    void CheckSceneView()
+    {
+        DeferredMSAA deferredAA = target as DeferredMSAA;
+        deferredAA.enabled = !isSceneView;
+        isSceneView = false;
+    }
+}
